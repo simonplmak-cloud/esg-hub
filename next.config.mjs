40f3c2dc3aabebfc635b1e970cc9399f7cf8f0dd
Simon Mak <simon.pl.mak@gmail.com>
2026-02-23 17:39:17 +0800

    Fix YouTube thumbnail images by adding img.youtube.com to remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        pathname: '/vi/**',
+      },
+    ],
+  },
   serverExternalPackages: [
     "onnxruntime-node",
     "sharp",
